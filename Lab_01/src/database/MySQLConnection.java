735d2a325a555ac30182ceae31a202ecbf4daea9
TuyenGT4 <tuyen1907004@gmail.com>
2024-10-27 23:20:31 +0700

    .

--- Lab_01/src/database/MySQLConnection.java
+++ Lab_01/src/database/MySQLConnection.java
@@ -1,5 +1,24 @@
 package database;
 
-public class MySQLConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class MySQLConnection {
+	private static String URL = "jdbc:mysql://localhost:3306/quan_ly_san_pham";
+    private static String USERNAME = "root";
+    private static String PASSWORD = "";
+
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connect successfully!");
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Connect failure!");
+            ex.printStackTrace();
+        }
+        return con;
+    }
 }
